1ca81b3a99d78b7fdee7f6b479ae40e40d160d70
Krish Dholakiya <krishna.dholakiya@gmail.com>
2018-06-15 10:05:29 -0400

    added sentience

--- src/chef.js
+++ src/chef.js
@@ -15,6 +15,35 @@ function main() {
   controller.on(['bot_channel_join', 'direct_mention'], (bot, event) => {
   	bot.reply(event, 'hey gang')
   })
+  
+  controller.hears(['if i say'], (bot, message) => {
+    const msgText = message.text;
+
+    if (!msgText.includes('you say')) {
+      return bot.reply(message, 'can u not ☹️')
+    }
+
+    const trigger = msgText.split('if i say')[1].split('you say')[0].trim()
+    const response = msgText.split('if i say')[1].split('you say')[1].trim()
+
+    controller.storage.commands.save({
+      trigger: trigger,
+      response: response, 
+      id: `${message.user}--${message.ts}`,
+    })
+
+    bot.reply(message, 'aight 😏')
+  })
+
+  if (controller.storage.commands) {
+    controller.storage.commands.all((err, commands) => {
+      commands.forEach(command => {
+        controller.hears([command.trigger], (bot, message) => {
+          bot.reply(message, command.response)
+        })
+      })
+    })
+  }
 }
 
 main()
